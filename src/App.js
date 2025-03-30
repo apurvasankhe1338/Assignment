611e4dae0401c38d874453f12412de6b88d58f56
apurvasankhe1338 <apurvaasankhe1338@gmail.com>
2025-03-30 11:53:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Matrix from './Components/Matrix';
+
+function App() {
+  return (
+    <div className="App">
+      <Matrix/>
+    </div>
+  );
+}
+
+export default App;
